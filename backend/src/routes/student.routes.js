428857195c3466f7038cdacb9050e680cf995313
samriddhi-21g <samriddhi.21g@gmail.com>
2026-05-05 13:30:15 +0530

    Backend and Database files removed from frontend

--- backend/src/routes/student.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const studentController = require('../controllers/student.controller');
-const { authenticate, authorize } = require('../middleware/auth');
-
-router.get('/me', authenticate, authorize('Student'), studentController.getProfile);
-
-module.exports = router;
